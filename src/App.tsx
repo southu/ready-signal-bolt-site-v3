c7ee7d584f71d77e4b46ff9ea51c4de26a574692
southu <jsnhrpr@gmail.com>
2026-01-17 21:00:02 -0500

    Add routes for all documentation pages - fixes broken brief-introduction page

--- src/App.tsx
+++ src/App.tsx
@@ -40,6 +40,30 @@ import B2BMarketing from './pages/industries/B2BMarketing';
 import B2CMarketing from './pages/industries/B2CMarketing';
 import AdminBlog from './pages/AdminBlog';
 
+// Documentation pages
+import BriefIntroduction from './pages/docs/BriefIntroduction';
+import HowToCreateSignal from './pages/docs/HowToCreateSignal';
+import CreateSignalVideo from './pages/docs/CreateSignalVideo';
+import DataScienceTreatments from './pages/docs/DataScienceTreatments';
+import DataGrainsExplained from './pages/docs/DataGrainsExplained';
+import ExportData from './pages/docs/ExportData';
+import ReduceSignalSize from './pages/docs/ReduceSignalSize';
+import FeatureDetails from './pages/docs/FeatureDetails';
+import DomoConnector from './pages/docs/DomoConnector';
+import DomoCaseStudy from './pages/docs/DomoCaseStudy';
+import ZipCodeTable from './pages/docs/ZipCodeTable';
+import StateAbbreviations from './pages/docs/StateAbbreviations';
+import FIPSCodes from './pages/docs/FIPSCodes';
+import ElectoralCollege from './pages/docs/ElectoralCollege';
+import SP500Companies from './pages/docs/SP500Companies';
+import BoxCoxTransformation from './pages/docs/BoxCoxTransformation';
+import YeoJohnsonTransformation from './pages/docs/YeoJohnsonTransformation';
+import LogarithmicTransformation from './pages/docs/LogarithmicTransformation';
+import OrderNormTransformation from './pages/docs/OrderNormTransformation';
+import SeasonalAdjustment from './pages/docs/SeasonalAdjustment';
+import AdvertisingAdstock from './pages/docs/AdvertisingAdstock';
+import RVideoExample from './pages/docs/RVideoExample';
+
 // Blog system - data-driven articles
 import BlogArticle from './pages/blog/BlogArticle';
 
@@ -90,6 +114,56 @@ function App() {
           <Route path="/ai-bi-consulting/" element={<AIBIConsulting />} />
           <Route path="/help-center" element={<HelpCenter />} />
           <Route path="/help-center/" element={<HelpCenter />} />
+
+          {/* Documentation / Help Articles */}
+          <Route path="/ready-signal-a-brief-introduction" element={<BriefIntroduction />} />
+          <Route path="/ready-signal-a-brief-introduction/" element={<BriefIntroduction />} />
+          <Route path="/how-to-create-a-signal" element={<HowToCreateSignal />} />
+          <Route path="/how-to-create-a-signal/" element={<HowToCreateSignal />} />
+          <Route path="/how-to-create-a-signal-video-version" element={<CreateSignalVideo />} />
+          <Route path="/how-to-create-a-signal-video-version/" element={<CreateSignalVideo />} />
+          <Route path="/overview-of-data-science-treatments" element={<DataScienceTreatments />} />
+          <Route path="/overview-of-data-science-treatments/" element={<DataScienceTreatments />} />
+          <Route path="/data-grains-explained" element={<DataGrainsExplained />} />
+          <Route path="/data-grains-explained/" element={<DataGrainsExplained />} />
+          <Route path="/how-to-export-your-processed-control-data-signal" element={<ExportData />} />
+          <Route path="/how-to-export-your-processed-control-data-signal/" element={<ExportData />} />
+          <Route path="/how-to-reduce-the-size-of-your-signal" element={<ReduceSignalSize />} />
+          <Route path="/how-to-reduce-the-size-of-your-signal/" element={<ReduceSignalSize />} />
+          <Route path="/ready-signal-feature-details" element={<FeatureDetails />} />
+          <Route path="/ready-signal-feature-details/" element={<FeatureDetails />} />
+          <Route path="/domo-data-connector" element={<DomoConnector />} />
+          <Route path="/domo-data-connector/" element={<DomoConnector />} />
+          <Route path="/ready-signal-and-domo-case-study" element={<DomoCaseStudy />} />
+          <Route path="/ready-signal-and-domo-case-study/" element={<DomoCaseStudy />} />
+          <Route path="/united-states-zip-code-data-table" element={<ZipCodeTable />} />
+          <Route path="/united-states-zip-code-data-table/" element={<ZipCodeTable />} />
+          <Route path="/us-state-abbreviations-data-table" element={<StateAbbreviations />} />
+          <Route path="/us-state-abbreviations-data-table/" element={<StateAbbreviations />} />
+          <Route path="/united-states-fips-codes-data-table" element={<FIPSCodes />} />
+          <Route path="/united-states-fips-codes-data-table/" element={<FIPSCodes />} />
+          <Route path="/united-states-electoral-college-data-table" element={<ElectoralCollege />} />
+          <Route path="/united-states-electoral-college-data-table/" element={<ElectoralCollege />} />
+          <Route path="/sp-500-companies-data-table" element={<SP500Companies />} />
+          <Route path="/sp-500-companies-data-table/" element={<SP500Companies />} />
+          <Route path="/box-cox-transformation" element={<BoxCoxTransformation />} />
+          <Route path="/box-cox-transformation/" element={<BoxCoxTransformation />} />
+          <Route path="/yeo-johnson-transformation" element={<YeoJohnsonTransformation />} />
+          <Route path="/yeo-johnson-transformation/" element={<YeoJohnsonTransformation />} />
+          <Route path="/logarithmic-transformation" element={<LogarithmicTransformation />} />
+          <Route path="/logarithmic-transformation/" element={<LogarithmicTransformation />} />
+          <Route path="/order-norm-transformation" element={<OrderNormTransformation />} />
+          <Route path="/order-norm-transformation/" element={<OrderNormTransformation />} />
+          <Route path="/seasonal-adjustment" element={<SeasonalAdjustment />} />
+          <Route path="/seasonal-adjustment/" element={<SeasonalAdjustment />} />
+          <Route path="/advertising-adstock" element={<AdvertisingAdstock />} />
+          <Route path="/advertising-adstock/" element={<AdvertisingAdstock />} />
+          <Route path="/ready-signal-api-documentation-r-3-6" element={<RVideoExample />} />
+          <Route path="/ready-signal-api-documentation-r-3-6/" element={<RVideoExample />} />
+          <Route path="/ready-signal-api-documentation" element={<APIDocs />} />
+          <Route path="/ready-signal-api-documentation/" element={<APIDocs />} />
+          <Route path="/ready-signal-api-documentation-python-sdk" element={<APIDocsPython />} />
+          <Route path="/ready-signal-api-documentation-python-sdk/" element={<APIDocsPython />} />
 
           {/* Legal pages */}
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,10 @@
 import { StrictMode } from 'react';
-import { createRoot, hydrateRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-const container = document.getElementById('root')!;
-
-// If the container has child nodes, it means the page was pre-rendered
-// Use hydrateRoot to attach React to the existing HTML
-if (container.hasChildNodes()) {
-  hydrateRoot(
-    container,
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-} else {
-  // Otherwise, render normally (development or non-pre-rendered pages)
-  createRoot(container).render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-}
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
